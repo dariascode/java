df13bf3a5e0bc2cd540fc80a220b02d22b3159f7
Dasha <m4daria@gmail.com>
2025-07-15 15:24:26 +0100

    hometasks

--- src/Lesson5.java
+++ src/Lesson5.java
@@ -1,3 +1,20 @@
-public class Lesson5 {
+import java.util.Scanner;
 
+public class Lesson5 {
+   public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+
+       double r;
+       System.out.print("Enter radius: ");
+       if (sc.hasNextDouble()) {
+           r = sc.nextDouble();
+       }
+       else {
+           System.out.println("You entered a non-numeric value");
+           return;
+       }
+
+       double c = (double) Math.round(Math.PI * 2 * r *100)/100;
+       System.out.println("The C of the circle is " + c);
+   }
 }
